e11c31cb85cae121a51fc07632c33574175aa43c
Galahad2819 <akhilnagar19@gmail.com>
2021-08-29 16:36:01 +0530

    Upload Done

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.homepage, name="homepage"),
     path('accounts/', include('allauth.urls')),
+    path('upload/', views.upload, name="upload")
 ]
--- website/views.py
+++ website/views.py
@@ -10,3 +10,14 @@ def homepage(request):
     video= Video.objects.all()
     return render(request,'homepage.html',{"video":video,})
 
+def upload(request):
+    if request.method=="POST":
+        title= request.POST["title"]
+        desc = request.POST['desc']
+        thumbnail =  request.FILES['thumbnail']
+        video =  request.FILES['video']
+        videoobj=Video(user=request.user,title=title,description=desc, thumbnail=thumbnail,video=video)
+
+
+    return render(request,'upload.html',)
+
